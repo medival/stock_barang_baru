691fbeed1454f8f587c52e0e01171fb1cdadbbc0
Restyled.io <commits@restyled.io>
2023-06-18 13:46:41 +0000

    Restyled by whitespace

--- application/models/M_barang.php
+++ application/models/M_barang.php
@@ -46,7 +46,7 @@ class M_barang extends CI_Model
 
         foreach ($this->column_search as $item) { // loop column
             if ($_POST['search']['value']) { // Jika datatable mengirim POST untuk search
-                
+
                 if ($i === 0) { // first loop
                     $this->db->group_start(); // open bracket.
 
@@ -63,7 +63,7 @@ class M_barang extends CI_Model
         }
 
         if (isset($_POST['order'])) { // Proses order
-            
+
             $this->db->order_by($this->column_order[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);
         } elseif (isset($this->order)) {
 
