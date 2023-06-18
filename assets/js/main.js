9b66991117cc97ddf23b1aac0fe887f12d617e83
Restyled.io <commits@restyled.io>
2023-06-18 13:42:52 +0000

    Restyled by clang-format

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,14 @@
 (function($) {
 
-	"use strict";
+"use strict";
 
-	var fullHeight = function() {
+var fullHeight = function() {
+  $('.js-fullheight').css('height', $(window).height());
+  $(window).resize(
+      function() { $('.js-fullheight').css('height', $(window).height()); });
+};
+fullHeight();
 
-		$('.js-fullheight').css('height', $(window).height());
-		$(window).resize(function(){
-			$('.js-fullheight').css('height', $(window).height());
-		});
-
-	};
-	fullHeight();
-
-	$('#sidebarCollapse').on('click', function () {
-      $('#sidebar').toggleClass('active');
-  });
-
+$('#sidebarCollapse')
+    .on('click', function() { $('#sidebar').toggleClass('active'); });
 })(jQuery);
